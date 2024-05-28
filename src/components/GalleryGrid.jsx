87da69608d42d544d2c88f415a67c567965e91ea
mohammad riza <abdulrazakumer@gmail.com>
2024-05-28 15:24:56 +0530

    gallery updated

--- src/components/GalleryGrid.jsx
+++ src/components/GalleryGrid.jsx
@@ -12,51 +12,65 @@ import gallery10 from "../assets/gallery-10.jpg";
 import gallery11 from "../assets/gallery-11.jpg";
 import gallery12 from "../assets/gallery-12.jpg";
 function GalleryGrid() {
-  const imgDivStyle = "flex justify-center items-center ";
-  const imgStyles = "h-full max-w-full rounded-lg object-fill object-center";
+  const data = [
+    {
+      imgelink: gallery1
+    },
+    {
+      imgelink: gallery2
+    },
+    {
+      imgelink:gallery3
+    },
+    {
+      imgelink: gallery4
+    },
+    {
+      imgelink: gallery5
+    },
+    {
+      imgelink: gallery6
+    },
+    {
+      imgelink: gallery7
+    },
+    {
+      imgelink: gallery8
+    },
+    {
+      imgelink: gallery9
+    },
+    {
+      imgelink: gallery11
+    },
+    {
+      imgelink: gallery12
+    },
+  ];
 
+  const [active, setActive] = React.useState(
+    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  );
   return (
-    <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-      <div className="grid gap-4">
-        <div>
-          <img className={imgStyles} src={gallery1} alt="gallery-photo" />
-        </div>
-        <div>
-          <img className={imgStyles} src={gallery2} alt="gallery-photo" />
-        </div>
-        <div>
-          <img className={imgStyles} src={gallery3} alt="gallery-photo" />
-        </div>
+    <div className="grid gap-4 justify-center">
+      <div className=" flex justify-center items-center max-w-7xl mx-auto">
+        <img
+          className="h-auto w-full max-w-full rounded-lg object-cover object-center md:max-h-[600px]"
+          src={active}
+          alt=""
+        />
       </div>
-      <div className="grid gap-4">
-        <div>
-          <img className={imgStyles} src={gallery4} alt="gallery-photo" />
-        </div>
-        <div>
-          <img className={imgStyles} src={gallery5} alt="gallery-photo" />
-        </div>
-        <div>
-          <img className={imgStyles} src={gallery6} alt="gallery-photo" />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div className={imgDivStyle}>
-          <img className={imgStyles} src={gallery7} alt="gallery-photo" />
-        </div>
-        <div className={imgDivStyle}>
-          <img className={imgStyles} src={gallery8} alt="gallery-photo" />
-        </div>
-        <div className={imgDivStyle}>
-          <img className={imgStyles} src={gallery9} alt="gallery-photo" />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <img src={gallery12} className={imgStyles} alt="gallery-photo" />
-        </div>
-        <div>
-          <img className={imgStyles} src={gallery11} alt="gallery-photo" />
-        </div>
+      <div className="grid grid-cols-5 gap-4">
+        {data.map(({ imgelink }, index) => (
+          <div key={index}>
+            <img
+              onClick={() => setActive(imgelink)}
+              src={imgelink}
+              className="h-full w-full max-w-full cursor-pointer rounded-lg object-cover object-center max-h-[360px]"
+              alt="gallery-image"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
