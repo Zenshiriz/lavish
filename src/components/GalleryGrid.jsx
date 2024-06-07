6aedbc168ccb7efdd8197d919229000189cd065b
mohammad riza <abdulrazakumer@gmail.com>
2024-06-07 16:33:10 +0530

    made the service page and fixed some bugs

--- src/components/GalleryGrid.jsx
+++ src/components/GalleryGrid.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { motion,layout } from "framer-motion";
+import React, { useEffect } from "react";
+import { motion} from "framer-motion";
+
 function GalleryGrid({images}) {
   
-
   const [active, setActive] = React.useState(images[0]);
+  useEffect(()=>{
+    setActive(images[0]);
+  },[images])
   return (
     <div className="grid gap-4 justify-center">
-      <div id="#img" className=" flex justify-center items-center max-w-7xl mx-auto" layout>
+      <motion.div id="#img" className=" flex justify-center items-center max-w-7xl mx-auto" layout >
         <img
           className="h-auto w-full max-w-full rounded-lg object-cover object-center md:max-h-[600px] transition-all duration-300 ease-in-out"
           src={active}
           alt=""
         />
-      </div>
+      </motion.div>
       <motion.div
       inherit={{opacity:0}}
       animate={{opacity:1}}
